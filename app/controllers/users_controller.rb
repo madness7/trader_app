174cbc677f2a5908c08ff515b30ab95e01eedc30
Graham Curl <gac.curl@gmail.com>
2014-09-09 11:13:22 +0100

    devise installed

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,25 @@
+class UsersController < Devise::RegistrationsController
+  before_filter :authenticate_user!
+
+
+  def index
+    @users = User.all
+  end
+
+  def create
+    # run default version defined in Devise::RegistrationsController
+    super
+
+    # then add our custom logic
+    @user.role = "user"
+    @user.save!
+  end
+
+  def edit
+    # log the ancestors of this controller
+    # to confirm that we inherit from ApplicationController
+    logger.info self.class.ancestors
+    super
+  end
+
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TraderApp::Application.config.secret_token = 'de8da3357fc18d78610bfd02c981a67ab932e396fd48815ad49b4a7532a0b8d9cba806ba2ef18b0656bf1c25bbb1297a5dfacfe4a3ea03d2c8c4d80e2a8da5f5'

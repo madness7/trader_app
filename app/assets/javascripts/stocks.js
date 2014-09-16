ebaf084b48082a157295c9b496ab37e206d850a8
Graham Curl <gac.curl@gmail.com>
2014-09-16 23:29:05 +0100

    JQuery stuff now works and the transaction total is being appending into the form when the quantity is increased. This is recieving up to date data from YF

--- app/assets/javascripts/stocks.js
+++ app/assets/javascripts/stocks.js
@@ -1,12 +1,14 @@
 $(function() {
     
     $('#portstock_quantity').on('change', function(){
-     console.log($('#portstock_quantity').val())
-     console.log($('#current_price').val())
-     //Get the value out of the input box - done
-     //Get the value of the stock - done
-     //Multiply together
-     //prepend it after the yahoo-finance stuff on the page
+     var quantity = parseInt($('#portstock_quantity').val());
+     var currentPrice = parseFloat($('#current_price').val());
+
+    if (!isNaN(quantity) && !isNaN(currentPrice)){
+        console.log("hello World");
+        var transaction_total = quantity * currentPrice;
+        $("#transaction_total").text(transaction_total);
+    }
     })
-    var transaction_total = '#portstock_quantity' * '#current_price' 
+    
 });

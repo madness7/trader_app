dc49e39355c567c356757b430a78d778ecaebbbb
Graham Curl <gac.curl@gmail.com>
2014-09-16 22:54:48 +0100

    changes made to page layout and screens

--- app/assets/javascripts/stocks.js
+++ app/assets/javascripts/stocks.js
@@ -2,13 +2,11 @@ $(function() {
     
     $('#portstock_quantity').on('change', function(){
      console.log($('#portstock_quantity').val())
-     //console.log('@current_price')
      console.log($('#current_price').val())
-     //<p id="current_price">Current price: <%= @current_price %>
-     //Get the value out of the input box
-     //Get the value of the stock
+     //Get the value out of the input box - done
+     //Get the value of the stock - done
      //Multiply together
-     //Append onto the page
+     //prepend it after the yahoo-finance stuff on the page
     })
-
+    var transaction_total = '#portstock_quantity' * '#current_price' 
 });
